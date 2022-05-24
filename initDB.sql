bd9ea2b39ff53d91d6ccd9ed7daf0644e87603c4
andy <pauchokbk@gmail.com>
2022-05-24 15:12:14 +0300

    Add all needed tables

--- initDB.sql
+++ initDB.sql
@@ -1,27 +1,67 @@
-CREATE database goit_javacore_hw3_andrii_zerko;
+CREATE TABLE developers (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(60) NOT NULL,
+    `second_name` VARCHAR(60) NOT NULL,
+    `birth_date` DATETIME,
+    `status` BOOLEAN NOT NULL
+);
+  
+CREATE TABLE skills (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `scope` VARCHAR(60) NOT NULL,
+    `level` INT NOT NULL,
+    `status` BOOLEAN NOT NULL
+);
 
-CREATE TABLE goit_javacore_hw3_andrii_zerko.developers (
-  `id` INT NOT NULL,
-  `first_name` VARCHAR(60) NOT NULL,
-  `last_name` VARCHAR(60) NOT NULL,
-  `birth_date` DATETIME,
-  `status` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE developers_skills (
+    `developer_id` INT NOT NULL,
+    `skill_id` INT NOT NULL,
+    INDEX (developer_id),
+    INDEX (skill_id),
+    FOREIGN KEY (developer_id)
+        REFERENCES developers (id),
+    FOREIGN KEY (skill_id)
+        REFERENCES skills (id)
+);
+
+CREATE TABLE companies (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(128) NOT NULL,
+    `status` BOOLEAN NOT NULL,
+    `date_start` DATE NOT NULL
+);
+
+CREATE TABLE customers (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(128) NOT NULL,
+    `second_name` VARCHAR(128) NOT NULL,
+    `status` BOOLEAN NOT NULL
+);
   
-CREATE TABLE goit_javacore_hw3_andrii_zerko.skills (
-  `id` INT NOT NULL,
-  `scope` VARCHAR(60) NOT NULL,
-  `level` INT NOT NULL,
-  `status` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`));
-  
-CREATE TABLE goit_javacore_hw3_andrii_zerko.projects (
-  `id` INT NOT NULL,
-  `company_id` INT NOT NULL,
-  `customer_id` INT NOT NULL,
-  `name` VARCHAR(128) NOT NULL,
-  `budget` FLOAT(5,2) NOT NULL,
-  `status` TINYINT(1) NOT NULL,
-  `date_start` DATE NOT NULL,
-  `date_end` DATE,
-  PRIMARY KEY (`id`));
+CREATE TABLE projects (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `company_id` INT NOT NULL,
+    `customer_id` INT NOT NULL,
+    `name` VARCHAR(128) NOT NULL,
+    `budget` FLOAT(2) NOT NULL,
+    `status` BOOLEAN NOT NULL,
+    `date_start` DATE NOT NULL,
+    `date_end` DATE,
+    INDEX company_id_idx (company_id),
+    INDEX customer_id_idx (customer_id),
+    CONSTRAINT fk_projects_companies FOREIGN KEY (company_id)
+        REFERENCES companies (id),
+    CONSTRAINT fk_projects_customers FOREIGN KEY (customer_id)
+        REFERENCES customers (id)
+);
+
+CREATE TABLE developers_projects (
+    `developer_id` INT NOT NULL,
+    `project_id` INT NOT NULL,
+    INDEX (developer_id),
+    INDEX (project_id),
+    FOREIGN KEY (developer_id)
+        REFERENCES developers (id),
+    FOREIGN KEY (project_id)
+        REFERENCES projects (id)
+);
